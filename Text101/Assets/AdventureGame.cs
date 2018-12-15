bf576f7acc485b2a24eae2c2acba9fa92c29aac4
Tyler Sweeney <tylersweeney@gmail.com>
2018-12-14 17:48:26 -0800

    initialize

---
+++ Text101/Assets/AdventureGame.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour {
+
+    [SerializeField] Text textComponent;
+
+	// Use this for initialization
+	void Start () {
+        textComponent.text = "Begin the Drongo Congo";
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
